0625fefcdb1ba4965b71527fba3eeb1e74c5d296
David THIBAU <david.thibau@gmail.com>
2024-03-06 10:55:48 +0100

    Contexte Spring

---
+++ tp1-sb2/src/test/java/org/formation/TestContext.java
@@ -0,0 +1,19 @@
+package org.formation;
+
+import org.junit.Test;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+
+public class TestContext {
+
+	
+	@Test
+	public void contextLoad() {
+		
+		ClassPathXmlApplicationContext context = new ClassPathXmlApplicationContext("/simple-job-launcher-context.xml");
+		
+		for ( String beanName : context.getBeanDefinitionNames() ) {
+			System.out.println("Bean :" + beanName);
+		}
+		
+	}
+}
