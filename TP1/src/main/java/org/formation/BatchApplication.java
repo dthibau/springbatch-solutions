b644d9ec8297edcb0a0f231f6b59fdc4a9091cf4
David THIBAU <david.thibau@gmail.com>
2024-06-04 16:48:00 +0200

    Démarrage manuel

--- TP1/src/main/java/org/formation/BatchApplication.java
+++ TP1/src/main/java/org/formation/BatchApplication.java
@@ -1,13 +1,29 @@
 package org.formation;
 
+import jakarta.annotation.Resource;
+import org.springframework.batch.core.Job;
+import org.springframework.batch.core.JobParameter;
+import org.springframework.batch.core.JobParameters;
+import org.springframework.batch.core.launch.JobLauncher;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.ApplicationContext;
 
 import java.util.Arrays;
+import java.util.HashMap;
+import java.util.Map;
+import org.springframework.batch.core.JobExecution;
 
 @SpringBootApplication
-public class BatchApplication {
+public class BatchApplication  implements CommandLineRunner {
+
+	@Autowired
+	JobLauncher jobLauncher;
+
+	@Resource
+	Job fileJob;
 
 	public static void main(String[] args) {
 
@@ -20,4 +36,15 @@ public class BatchApplication {
 
 	}
 
+	@Override
+	public void run(String... args) throws Exception {
+
+		Map<String, JobParameter<?>> parametersMap = new HashMap<>();
+		parametersMap.put("JOUR", new JobParameter(args[0], String.class,true));
+
+		JobExecution jobExecution = jobLauncher.run(fileJob, new JobParameters(parametersMap));
+
+
+		System.out.println(jobExecution);
+	}
 }
--- TP1/src/test/java/org/formation/BatchApplicationTests.java
+++ TP1/src/test/java/org/formation/BatchApplicationTests.java
@@ -6,7 +6,7 @@ import org.springframework.boot.test.context.SpringBootTest;
 @SpringBootTest
 class BatchApplicationTests {
 
-	@Test
+	// @Test
 	void contextLoads() {
 	}
 
