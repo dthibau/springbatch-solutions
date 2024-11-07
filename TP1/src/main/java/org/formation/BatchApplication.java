afb86e01c31763c8c39a65975d527063a83e3c8e
David THIBAU <david.thibau@gmail.com>
2024-11-07 10:50:43 +0100

    Refactoring

--- TP1/src/main/java/org/formation/BatchApplication.java
+++ TP1/src/main/java/org/formation/BatchApplication.java
@@ -7,11 +7,11 @@ import org.springframework.context.ApplicationContext;
 import java.util.Arrays;
 
 @SpringBootApplication
-public class Tp1Application {
+public class BatchApplication {
 
 	public static void main(String[] args) {
 
-		ApplicationContext context = SpringApplication.run(Tp1Application.class, args);
+		ApplicationContext context = SpringApplication.run(BatchApplication.class, args);
 
 		Arrays.asList(context.getBeanDefinitionNames()).stream()
 				.filter(n -> n.toLowerCase().contains("job") || n.toLowerCase().contains("step") || n.toLowerCase().contains("batch") )
